30b93a379a5afd96a53e2a9dd372bf057c78b3ca
DC123456789 <darrenchan611991@gmail.com>
2020-01-24 01:24:01 -0500

    - (Possibly) fixed a bug where temporary revolt titles would not be destroyed properly and then could be recreated - Increased max possible shattered retreat length and morale recovery - Adjusted creation requirements for Elmetsaete and East Anglia - Nerfed the stats of several event-spawned mercenary leader characters

--- Britannia/common/defines.lua
+++ Britannia/common/defines.lua
@@ -1460,10 +1460,10 @@ NMilitary = {
 	SHATTERED_RETREAT_FRIENDLY_ATTRITION = 0,	-- Attrition taken when traversing friendly territory during a shattered retreat (as a percentage)
 	
 	SHATTERED_RETREAT_MOVEMENT_MULTIPLIER = 1.2,-- Multiplier of unit movement speed when shattered
-	SHATTERED_RETREAT_MORALE_MULTIPLIER = 2,	-- Multiplier of morale regain when shattered
+	SHATTERED_RETREAT_MORALE_MULTIPLIER = 2.5,	-- Multiplier of morale regain when shattered
 	
 	SHATTERED_RETREAT_PREFERRED_PROVINCES = 4,				-- Units will try to move at least this many provinces away in shattered retreat
-	SHATTERED_RETREAT_MAX_PROVINCES = 6,					-- Units can not move longer than this many provinces during a shattered retreat
+	SHATTERED_RETREAT_MAX_PROVINCES = 8,					-- Units can not move longer than this many provinces during a shattered retreat
 	SHATTERED_RETREAT_DISTANCE_MULTIPLIER = -20,			-- Value added for each province away from the prefered distance (whether too close or too far)
 	SHATTERED_RETREAT_OWN_UNIT_MULTIPLIER = 0.1,			-- Multiplier of own units in province when evaluating retreat provinces
 	SHATTERED_RETREAT_WAR_FRIEND_UNIT_MULTIPLIER = 0.025,	-- Multiplier of war friend units in province when evaluating retreat provinces
